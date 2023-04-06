4866470d48d2677106c2eae02140d87cd1adb1fe
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-04-06 11:15:24 +0100

    added devops pipeline

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ sudo npm install -g pm2
 pm2 stop devapp
 cd devapp/
 npm install
-npm run build
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 pm2 start ./bin/server.js --name devapp
